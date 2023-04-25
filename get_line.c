c6685004b72aa3b0bb3ee0f4df2fa60530420d4a
Haidyasser <haidyasser3@gmaail.com>
2023-04-26 01:21:55 +0200

    solve conflict

--- get_line.c
+++ get_line.c
@@ -1,53 +1,11 @@
 #include "shell.h"
-<<<<<<< HEAD
-=======
 
->>>>>>> 39d25c9 (betty style)
 /**
  * _getline - get a line from stdin
  * @line: pointer to a pointer to a string
  * Return: number of characters read
  * On error: -1 inapropiate entry
  */
-<<<<<<< HEAD
-int _getline(char **line)
-{
-        int i = 0, size = 0, nread = 0;
-        char *buffer = NULL;
-        if (line == NULL)
-                return (-1);
-        buffer = malloc(sizeof(char) * 1024);
-        if (buffer == NULL)
-                return (-1);
-        nread = read(STDIN_FILENO, buffer, 1024);
-        if (nread == 0)
-        {
-                free(buffer);
-                return (-1);
-        }
-        while (buffer[i] != '\n')
-        {
-                i++;
-                size++;
-        }
-        *line = malloc(sizeof(char) * (size + 1));
-        if (*line == NULL)
-        {
-                free(buffer);
-                return (-1);
-        }
-        _strncpy(*line, buffer, size);
-        free(buffer);
-        return (size);
-}
-
-
-
-
-
-
-
-=======
 
 int _getline(char **line)
 {
@@ -80,4 +38,3 @@ int _getline(char **line)
 	free(buffer);
 	return (size);
 }
->>>>>>> 39d25c9 (betty style)
