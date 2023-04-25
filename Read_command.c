a872f6306649277cba7d40024fd0c293861222b8
Haidyasser <haidyasser3@gmaail.com>
2023-04-26 01:29:08 +0200

    handle ctrl + c

--- Read_command.c
+++ Read_command.c
@@ -1,6 +1,18 @@
 #include "shell.h"
 
 /**
+ * handler - handles the signal
+ * @sig: signal
+ * Return: void
+ */
+
+void handler(int sig)
+{
+	(void)sig;
+	write(STDOUT_FILENO, "\n>> ", 4);
+}
+
+/**
  * read_command - reads the command line
  * @nread: number of characters the user types
  * Return: the command line
@@ -9,8 +21,10 @@
 char *read_command(int *nread)
 {
 	char *buf;
+	size_t bufsize = 0;
 
-	*nread = _getline(&buf);
+	signal(SIGINT, handler);
+	*nread = getline(&buf, &bufsize, stdin);
 	if (*nread == -1)
 	{
 		if (feof(stdin))
