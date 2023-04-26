339cc841e8d066e68368833cf0ecff6fd9a86abc
Haidy Yasser <99363484+Haidyasser@users.noreply.github.com>
2023-04-26 21:23:43 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,25 +2,30 @@
 
 /**
  * main - main function
+ * @ac: number of arguments
+ * @av: array of arguments
  * Return: 0
  */
 
-int main(void)
+int main(int ac, char **av)
 {
 	char *command, **args;
 	int status = 1;
 	int nread; /* number of characters the user types */
 
+	(void)ac;
 	while (status)
 	{
-		display(">> ");
+		if (isatty(STDIN_FILENO))
+			display("$cisfun# ");
+		signal(SIGINT, handler);
+
 		command = read_command(&nread);
 		args = split_command(command, &nread);
-		status = excute(args);
+		status = excute(args, av);
 
 		free(command);
 		free(args);
 	}
 	return (0);
 }
-
